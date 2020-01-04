2098f03804da307cba8ab43fa508fa6698478fd5
Julian Rubisch <julian@julianrubisch.at>
2020-01-04 18:25:15 +0100

    Fix dangerously... prop structure

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { useCodeFundAd } from '@codefund/react-api-hook'
+import React from "react"
+import { useCodeFundAd } from "@codefund/react-api-hook"
 
 // TODO
-const { ad, loading, error } = useCodeFundAd('669')
-console.log(ad)
 const App = () => {
-  // return <Ad adMarkup={ad} loadingState={loading} errorMessage={error} />
+  const { ad, loading, error } = useCodeFundAd("669")
+  console.log(ad)
+  return <Ad adMarkup={ad} loadingState={loading} errorMessage={error} />
 }
 
-// const Ad = ({ adMarkup, loadingState, errorMessage }) => <div dangerouslySetInnerHTML={adMarkup} />
+const Ad = ({ adMarkup, loadingState, errorMessage }) => (
+  <div dangerouslySetInnerHTML={{ __html: adMarkup }} />
+)
 
 export default App
