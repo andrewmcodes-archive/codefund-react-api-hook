68405cf07da47c1af37d6e1ffdcaf18ac0d968d9
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-06 23:22:57 -0500

    wip

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,40 @@
 import React from "react"
+import Skeleton from "react-skeleton-loader"
 import { useCodeFundAd } from "@codefund/react-api-hook"
 
-// TODO
-const App = () => {
-  const { ad, loading, error } = useCodeFundAd("669")
-  console.log(ad)
-  return <Ad adMarkup={ad} loadingState={loading} errorMessage={error} />
+class App extends React.Component {
+  render () {
+    return (
+      <>
+        <p class="blokk">
+          BLOKK is a font for quick mock-ups and wireframing for clients who do not understand latin.
+        </p>
+        <div class="center">
+          <CodeFund />
+        </div>
+      </>
+    )
+  }
 }
 
-const Ad = ({ adMarkup, loadingState, errorMessage }) => (
-  <div dangerouslySetInnerHTML={{ __html: adMarkup }} />
-)
+
+function CodeFund () {
+  const { ad, loading, error } = useCodeFundAd("669")
+  if (loading) {
+    return <AdLoading />
+  }
+
+  if (!error) {
+    return <AdMarkup ad={ad} />
+  }
+}
+
+function AdMarkup (props) {
+  return <div dangerouslySetInnerHTML={{ __html: props.ad }} />
+}
+
+function AdLoading () {
+  return <Skeleton count={5} width={"330px"} borderRadius={"0px"} />
+}
 
 export default App
