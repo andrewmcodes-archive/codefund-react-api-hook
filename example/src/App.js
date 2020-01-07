5005cdb2a25b5e6ffc0e0b679d3c68961484c29d
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-06 23:39:41 -0500

    finish the demo

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,18 @@
 import React from "react"
 import Skeleton from "react-skeleton-loader"
+import { LoremIpsum } from "react-lorem-ipsum"
 import { useCodeFundAd } from "@codefund/react-api-hook"
 
 class App extends React.Component {
   render () {
     return (
       <>
-        <p class="blokk">
-          BLOKK is a font for quick mock-ups and wireframing for clients who do not understand latin.
-        </p>
+        <h1>@codefund/react-api-hook Demo</h1>
+        <Text paragraphs={2} />
         <div class="center">
           <CodeFund />
         </div>
+        <Text paragraphs={6} />
       </>
     )
   }
@@ -35,6 +36,14 @@ function AdMarkup (props) {
 
 function AdLoading () {
   return <Skeleton count={5} width={"330px"} borderRadius={"0px"} />
+}
+
+function Text (props) {
+  return(
+    <div className="text-wrapper">
+      <LoremIpsum p={props.paragraphs} avgSentencesPerParagraph={16} />
+    </div>
+  )
 }
 
 export default App
