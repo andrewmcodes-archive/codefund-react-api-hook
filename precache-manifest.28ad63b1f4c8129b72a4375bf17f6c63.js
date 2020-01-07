8aab8c48397fdd1dc6ac2b05b8f726c7f57fc9d0
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-07 02:16:13 -0500

    Updates

---
+++ precache-manifest.28ad63b1f4c8129b72a4375bf17f6c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c032cdcac6a979751f1f46dbeb5076e2",
+    "url": "/codefund-react-api-hook/index.html"
+  },
+  {
+    "revision": "02f3aeb9284c60aeea6c",
+    "url": "/codefund-react-api-hook/static/css/main.8e29e2e4.chunk.css"
+  },
+  {
+    "revision": "2d649ac148d81981756b",
+    "url": "/codefund-react-api-hook/static/js/2.1cb9cf69.chunk.js"
+  },
+  {
+    "revision": "d50198c2b3cb3b9437d9698faa7c978c",
+    "url": "/codefund-react-api-hook/static/js/2.1cb9cf69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02f3aeb9284c60aeea6c",
+    "url": "/codefund-react-api-hook/static/js/main.bfd1ea3d.chunk.js"
+  },
+  {
+    "revision": "5a1a3d77ce8b766e756a",
+    "url": "/codefund-react-api-hook/static/js/runtime-main.9c454c16.js"
+  }
+]);
